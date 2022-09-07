d26c3d8588b56cdb18efdaabbbc71c5d2a95ad90
ivoitov <ivoitov@purdue.edu>
2022-09-07 11:54:48 -0400

    CGT 215 Lab 3 Ivan Voitov

--- Lab3_Voitov_Ivan_CGT215/Lab3_Voitov_Ivan_CGT215.cpp
+++ Lab3_Voitov_Ivan_CGT215/Lab3_Voitov_Ivan_CGT215.cpp
@@ -1,20 +1,53 @@
-// Lab3_Voitov_Ivan_CGT215.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+}
+void getAddition(float A, float B) {
+	float C = A + B;
+	cout << "\n" << A << " + " << B << " = " << C << endl;
+}
+void getSubtraction(float A, float B) {
+	float C = A - B;
+	cout << "\n" << A << " - " << B << " = " << C << endl;
+}
+void getMultiplication(float A, float B) {
+	float C = A * B;
+	cout << "\n" << A << " * " << B << " = " << C << endl;
+}
+void getDivision(float A, float B) {
+	float C = A / B;
+	cout << "\n" << A << " / " << B << " = " << C << endl;
+} 
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		getAddition(A, B);
+	}
+	if (choice == 2) {
+		getSubtraction(A, B);
+	}
+	if (choice == 3) {
+		getMultiplication(A, B);
+	}
+	if (choice == 4) {
+		getDivision(A, B);
+	}
+	return 0;
+}
